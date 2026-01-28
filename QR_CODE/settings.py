203687ffaf6d4e34dc2d48a1934a84dd86ade806
imranworkspace <149460073+imranworkspace@users.noreply.github.com>
2026-01-28 18:53:55 +0530

    Update settings.py

--- QR_CODE/settings.py
+++ QR_CODE/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
